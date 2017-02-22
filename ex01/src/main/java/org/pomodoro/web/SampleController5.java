df178c30f7d5f4242ae82b861ebe2c456243d41b
pomodoro <jamesj4318@gmail.com>
2017-02-22 20:08:32 +0900

    create project ex01

---
+++ ex01/src/main/java/org/pomodoro/web/SampleController5.java
@@ -0,0 +1,18 @@
+package org.pomodoro.web;
+
+import org.pomodoro.domain.ProductVO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController5 {
+
+	@RequestMapping("/doJSON")
+	public @ResponseBody ProductVO doJSON() {
+		
+		ProductVO vo = new ProductVO("»ùÇÃ»óÇ°", 30000);
+		
+		return vo;
+	}
+}
